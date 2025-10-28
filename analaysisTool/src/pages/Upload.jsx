64004d711f57ff8cc8aa33dcb6826a8f8e1dd75e
Dory500 <114651417+Starry5464@users.noreply.github.com>
2025-10-28 22:48:41 +0000

    CA - fix upload page conflict

--- analaysisTool/src/pages/Upload.jsx
+++ analaysisTool/src/pages/Upload.jsx
@@ -1,12 +1,3 @@
 export default function Upload() {
-<<<<<<< Updated upstream
-  return (
-    <div>
-      <h1>Upload</h1>
-      <p>Upload a file to the backend and save sessions </p>
-    </div>
-  )
-=======
   return <FileUploader />;
->>>>>>> Stashed changes
 }
