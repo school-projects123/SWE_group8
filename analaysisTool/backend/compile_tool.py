c000061e1aaab318cd1e44cb9a5ebfb630f7827c
kayma0 <131287457+kayma0@users.noreply.github.com>
2025-11-14 18:24:52 +0000

    input and output setup

--- analaysisTool/backend/compile_tool.py
+++ analaysisTool/backend/compile_tool.py
@@ -1,3 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
+INPUT_DIRECTORY = Path("input_files")
+OUTPUT_DIRECTORY = Path("output")
+OUTPUT_DIRECTORY.mkdir(exist_ok=True)
+
+OUTPUT_FILE = OUTPUT_DIRECTORY / "Master_Spreadsheet.xlsx"
