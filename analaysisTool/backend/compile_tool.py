0ace6b9221c1c66ff6b3ebdf1cd248440cc963d5
kayma0 <131287457+kayma0@users.noreply.github.com>
2025-11-14 18:39:17 +0000

    Added gradebook file detection function

--- analaysisTool/backend/compile_tool.py
+++ analaysisTool/backend/compile_tool.py
@@ -6,3 +6,11 @@ OUTPUT_DIRECTORY = Path("output")
 OUTPUT_DIRECTORY.mkdir(exist_ok=True)
 
 OUTPUT_FILE = OUTPUT_DIRECTORY / "Master_Spreadsheet.xlsx"
+
+def detect_gradebook(df: pd.DataFrame):
+    columns = list(df.columns)
+    required_columns = {"First Name", "Last Name", "Username", "Student ID"}
+    has_all_columns = required_columns.issubset(set(columns))
+    has_course_grade = any("Course Grade Point to Date" in col for col in columns)
+    return has_all_columns and has_course_grade
+
