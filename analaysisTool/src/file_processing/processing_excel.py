56076a8f059617e0b5c512adaf2e58c4c617d40b
GillLart <158057623+GillLart@users.noreply.github.com>
2025-11-26 02:20:49 +0000

    G: adding sendInfo function to fileUploader, still need to implement course selection, updates python script to tak send infos output, chanded to form data as sending over entire files as is in a json would be insane, updating readme to add dev instructions, and a requirements.txt for python, (using python 3.14.0)

--- analaysisTool/src/file_processing/processing_excel.py
+++ analaysisTool/src/file_processing/processing_excel.py
@@ -1,67 +1,73 @@
 # need to change to snake case
 from flask import Flask, request, jsonify # flask for getting file path from front end (upload page)
+from flask_cors import CORS
 import os # for finding the file path
-import json # for mock retriving the post until that is set up in frontend
+import pandas as pd # for mock retriving the post until that is set up in frontend
 
 app = Flask(__name__)
+#to allow for cross platform communication (flask and vite are on diff [ports for development])
+CORS(app)
 
 # This script will retrive the information from the upload page and store/ sort the excel files depending on the courses they are assigned to
 
 # for now file info is being passed in the function directly to mock the json responce
-# file_info = {"num_of_files":"N", "file_0": {"file_path": "C:\efsj\nje", "course": "CS3400"}, "file_1": {"file_path": "C:\efsj\nje", "course": "CS3400"}, ..., "file_N":{}}
+# file_info = {"num_of_files":"N", "file_0": {"name": "name.csv","FormData": "what everform data looks like", "course": "CS3400"},..., "file_N":{}}
 # likely recived via a POST from front end that sends a json that can be read as a python dict /process with be the front end command thats sends the files to be processed once the gen report button is clicked
 @app.route("/process", methods=["POST"])
 
 # funtion to process input from react frontend
-def process_excel(file_info):
+def process_excel():
     #info = request.get_json
-    info = file_info
-    #num_of_files = int(info.get("num_of_files",0))
-    num_of_files = int(file_info["num_of_files"])
-    if num_of_files == 0: 
-        return jsonify({"error": "No files provided"}), 400
+    num_of_files = int(request.form.get("numOfFiles",0))
+    #num_of_files = int(file_info["num_of_files"])
+    courses = {}
 
-    results = []
     for i in range(num_of_files):
-        file_indx = f"file_{i+1}"
-        file_info = info.get(file_indx)
-        print(file_info)
-        # if for some reason theres other stuff in the json ignore it lol
-        if not file_info:
-            continue
 
         # could there ever be a case where the front end sends an empty filepath or course?
-        path = file_info.get("file_path")
-        course = file_info.get("course")
+        file = request.files.get(f"file_{i}")
+        course = request.form.get(f"course_{i}","unknown")
 
-        # incase it isnt a proper path/ file - unlikely beacsie of frontend safeguards
-        if not path or not os.path.exists(path):
-            results.append({
-                    "file_name": os.path.basename(path) if path else None,
-                    "course": course,
-                    "error": "File/folder does not exist"
+        if course not in courses:
+            courses[course] = []
+        # incase there isn't a course (because thats not implemented yet)
+        if file:
+            try:
+                # read excel/csv
+                file_extrac = file.filename.split(".")[-1].lower()
+                preview_text = ""
+
+                if file_extrac in ["csv"]:
+                    df = pd.read_csv(file)
+                    # currently preview first 5 rows
+                    preview_text = df.head(5).to_csv(index = False) 
+                elif file_extrac in ["xls","xlsx"]:
+                    df = pd.read_excel(file)
+                    preview_text = df.head(5).to_csv(ndex=False)
+                else:
+                    # error if another file is somehow uloaded
+                    content = file.read().decode("utf-8", errors = "ignore")
+                    preview_text = content[:500]
+
+                courses[course].append({
+                    "file_name":file.filename,
+                    "preview":preview_text
                 })
-            continue
-
-        # if it is a path
-        with open(path, "r", encoding="utf-8", errors="ignore") as f:
-            content_preview = f.read(500)  # first 500 chars
-        results.append({
-            "file_name": os.path.basename(path),
-            "course": course,
-            "type": "file",
-            "preview": content_preview
-        })
-
+            except Exception as e:
+                courses[course].append({
+                    "file_name": file.filename,
+                    "": f"failed to read file:{str(e)}"
+                })
         # likely wont need to handle if its a folder because of frontend safeguards
         # could the same file be uploaded twise? how to deal with it?
 
     #return jsonify({"status": "success", "results": results})
     # the temp filepaths probubly will be marked as not existing as they are gibberish
-    print(results)
+    print(courses)
+    return jsonify({"status":"success", "courses":courses})
 
 if __name__ =="__main__":
     # app.run(port=5000, debug=True)
     # dummy file_info dict to use for now
     file_info = {"num_of_files":"2", "file_1": {"file_path": "C:\\efsj\\nje.csv", "course": "CS3400"}, "file_2": {"file_path": "C:\\apdmj\\nefje.csv", "course": "CS3203"}}
-    process_excel(file_info)
+    app.run(port=5000,debug=True)
